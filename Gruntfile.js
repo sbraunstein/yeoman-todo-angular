37db888bbc4cdb53a3833ba19e6e1209f230a050
sbraunstein <sbraunstein12@gmail.com>
2015-08-11 12:59:40 -0500

    todo app finished

--- Gruntfile.js
+++ Gruntfile.js
@@ -319,7 +319,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'stevenbraunsteinApp',
+          module: 'mytodoApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,14 @@
 'use strict';
 
 angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+    localStorageService.set('todos', $scope.todos);
+    }, true);
+    
   	$scope.addTodo = function () {
   		$scope.todos.push($scope.todo);
   		$scope.todo = '';
@@ -9,5 +16,5 @@ angular.module('mytodoApp')
   	$scope.removeTodo = function (index) {
   		$scope.todos.splice(index, 1);
   	};
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+    $scope.todos = [];
   });
