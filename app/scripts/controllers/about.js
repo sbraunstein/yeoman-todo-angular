39f490af1ebe0ee25bcee47f3672e56759730159
sbraunstein <sbraunstein12@gmail.com>
2015-08-11 11:29:58 -0500

    added file structure and scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stevenbraunsteinApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the stevenbraunsteinApp
+ */
+angular.module('stevenbraunsteinApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stevenbraunsteinApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stevenbraunsteinApp
+ */
+angular.module('stevenbraunsteinApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
